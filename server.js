5d7fbe177cabaace84a656692d754c1c7a08faa4
Franco Quattrini <fquattrini01@gmail.com>
2021-09-04 19:17:54 -0300

    FINISH

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const app = express()
 const router = require("./routes/index")
 require("./config/database")
 require("./config/passport")
+const path = require("path")
 
 // Middleware
 app.use(cors())
 app.use(express.json())
 
 app.use("/api", router)
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"))
+   app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"))
+   })
+}
 
-app.listen(4000, () => console.log("Server running on port 4000"))
+app.listen(process.env.PORT || 4000, process.env.LOCALHOST || "0.0.0.0", () =>
+   console.log("Server running on port 4000")
+)
